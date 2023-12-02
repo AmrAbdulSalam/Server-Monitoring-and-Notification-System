f3ba9a923c69e9f49ccec9cd9c3ba40f46a49427
unknown <amrsalam@hotmail.com>
2023-12-02 21:17:36 +0800

    Adding RabbitMQ message broker to publish server statistics

---
+++ ServerStatisticsPublisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+
+namespace ServerStatisticsPublisher
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) 
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            await TopicPublisher.Publish(channel);
+        }
+    }
+}
---
+++ ServerStatisticsPublisher/TopicPublisher.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using Configurations;
+using ServerStatisticsService;
+
+namespace ServerStatisticsPublisher
+{
+    public static class TopicPublisher
+    {
+        public static async Task Publish(IModel channel)
+        {
+            channel.ExchangeDeclare("topic-ServerStatistics-exchange" , ExchangeType.Topic);
+            string? path = @"D:\intership\ServerMonitoringAndNotificationSystem\appsettings.json";
+
+            while (true)
+            {
+                await ReadConfigurations.ReadSettingsFile(path);
+
+                var systemDiagnostic = new SystemDiagnostic();
+                var message = systemDiagnostic.UpdateServerInfo();
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+  
+                var serverIdentifier = ReadConfigurations.Configurations.ServerStatisticsConfig.ServerIdentifier;
+                var samplingIntervalSeconds = ReadConfigurations.Configurations.ServerStatisticsConfig.SamplingIntervalSeconds*1000;
+
+                Console.WriteLine(message.ToString());
+                channel.BasicPublish("topic-ServerStatistics-exchange", $"ServerStatistics.{serverIdentifier}", null, body);
+                Thread.Sleep(samplingIntervalSeconds);
+            }
+        }
+    }
+}
