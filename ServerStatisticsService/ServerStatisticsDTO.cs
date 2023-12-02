8b188671fe87bd9285c2bc5bdf386a45de8e5c3f
unknown <amrsalam@hotmail.com>
2023-12-02 18:25:43 +0800

    Collects server (local machine) statistics

---
+++ ServerStatisticsService/ServerStatisticsDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ServerStatisticsService
+{
+    public class ServerStatisticsDTO
+    {
+        public double MemoryUsage { get; set; } //MB
+        public double AvailableMemory { get; set; } //MB
+        public double CpuUsage { get; set; }
+        public DateTime Timestamp { get; set; }
+        public override string ToString() 
+            => $"MemoryUsage:{MemoryUsage}%,AvailableMemory:{AvailableMemory},CpuUsage:{CpuUsage},Time:{Timestamp}";
+    }
+}
